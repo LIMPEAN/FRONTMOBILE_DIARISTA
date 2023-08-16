5624ac53c673cef7f152c287477d4088bb10e824
Felipe Florencio <107069825+FelipeFlorencio9@users.noreply.github.com>
2023-08-16 08:53:43 -0300

    Criado estrutura para o registro de diarista

---
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/register/RegisterPersonalScreen.kt
@@ -0,0 +1,27 @@
+package br.senai.sp.jandira.limpeanapp.register
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose.LimpeanAppTheme
+
+@Composable
+fun RegisterPersonalScreen() {
+    Column {
+//        SectionTitle(title = "Cadastro", description = "Contato")
+//        PersonalForm()
+//        Button(){}
+//        CircleDecoration()
+        
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun RegisterPersonalScreenPreview() {
+    LimpeanAppTheme {
+        RegisterPersonalScreen()
+
+    }
+}
