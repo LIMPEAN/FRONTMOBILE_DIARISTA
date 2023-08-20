3045c0e7b3fa80d25447f5944fdb8b5b32bbc505
Felipe Florencio <felipeoflorencio@hotmail.com>
2023-08-20 17:19:40 -0300

    Criado componente LabelError para prévia visualização de erros

---
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/components/LabelError.kt
@@ -0,0 +1,22 @@
+package br.senai.sp.jandira.limpeanapp.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LabelError(text: String) {
+    Text(
+        text = text,
+        color = MaterialTheme.colorScheme.error,
+        modifier = Modifier
+            .height(20.dp),
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+}
