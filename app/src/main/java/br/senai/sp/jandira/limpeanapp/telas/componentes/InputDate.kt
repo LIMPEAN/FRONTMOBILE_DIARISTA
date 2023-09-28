713712088d573192bf6f8b48fe0d232a9657c67c
Felipe Florencio <felipeoflorencio@hotmail.com>
2023-09-28 18:05:25 -0300

    Tentando fazer o InputDate

---
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/telas/componentes/InputDate.kt
@@ -0,0 +1,104 @@
+package br.senai.sp.jandira.limpeanapp.telas.componentes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
+
+@Composable
+fun InputDate(
+    initialDate: String,
+    onDateChanged: (String) -> Unit
+) {
+    var text by remember { mutableStateOf(TextFieldValue(initialDate)) }
+    var isFocused by remember { mutableStateOf(false) }
+
+    val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+    dateFormat.isLenient = false
+
+    val dateTransformation = DateTransformation()
+
+    //Outlined
+
+    if (!isFocused) {
+        Text(
+            text = text.text,
+            modifier = Modifier.clickable {
+                isFocused = true
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun InputDatePreview() {
+    var date by remember { mutableStateOf("") }
+    Column {
+        InputDate(
+            initialDate = date,
+            onDateChanged = { newDate -> date = newDate }
+        )
+        Text(text = "Data inserida: $date")
+    }
+}
+
+class DateTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        return dateFilter(text)
+    }
+}
+
+fun dateFilter(text: AnnotatedString): TransformedText {
+
+    val trimmed = if (text.text.length >= 8) text.text.substring(0..7) else text.text
+    var out = ""
+    for (i in trimmed.indices) {
+        out += trimmed[i]
+        if (i % 2 == 1 && i < 4) out += "/"
+    }
+
+    val numberOffsetTranslator = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            if (offset <= 1) return offset
+            if (offset <= 3) return offset +1
+            if (offset <= 8) return offset +2
+            return 10
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            if (offset <=2) return offset
+            if (offset <=5) return offset -1
+            if (offset <=10) return offset -2
+            return 8
+        }
+    }
+
+    return TransformedText(AnnotatedString(out), numberOffsetTranslator)
+}
+
