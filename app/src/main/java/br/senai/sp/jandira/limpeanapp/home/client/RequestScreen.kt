08cac69ba93535b8c3096e7193199b55abbe74e8
LETÍCIA EVELIN AGUIAR E SILVA <22282118@dsn127m1246727.cfp127aluno.local>
2023-10-05 11:05:07 -0300

    layout card serviço

--- app/src/main/java/br/senai/sp/jandira/limpeanapp/home/client/RequestScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/home/client/RequestScreen.kt
@@ -1,42 +1,75 @@
 package br.senai.sp.jandira.limpeanapp.home.client
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.compose.LimpeanAppTheme
+import com.example.compose.md_theme_light_onPrimary
 import com.example.compose.seed
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun TypeOfRequest(){
+fun TypeOfRequest() {
+
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
 
         topBar = {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(0.25f)
-                    .background(color = seed)
-                    .padding(start = 55.dp, bottom = 70.dp)
-                    .offset(x = 6.dp)
-                ,
+                    .padding(start = 24.dp, bottom = 70.dp)
+                    .offset(x = 6.dp),
                 verticalArrangement = Arrangement.Center
             ) {
 
@@ -53,20 +86,53 @@ fun TypeOfRequest(){
                                     fontSize = 28.sp
                                 )
                             ) {
-                                append("Loira")
+                                append("Leticia Wandinha")
                             }
                         }
-                    },
+                    }
                 )
-        }
 
             }
+        },
+       bottomBar = {
+           Text(text = "footer")
+       }
 
+    )
+    {
 
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(color = seed), verticalArrangement = Arrangement.Bottom
+        ) {
+            Spacer(modifier = Modifier.height(100.dp))
+            Card(
+                modifier = Modifier
+                    .fillMaxSize(),
+                colors = CardDefaults.cardColors(Color.White),
 
+                shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+            ) {
+                Spacer(modifier = Modifier.height(40.dp))
+                Text(text = "conteúdo")
 
-    ) {
-
+            }
+        }
 
     }
+
 }
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun RequestPreview() {
+    TypeOfRequest()
+}
+
+
+
+
+
