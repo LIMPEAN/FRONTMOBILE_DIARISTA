3dd4eff0ff1a0a7d4c3365d25bcfa61e545c786f
Felipe Florencio <107069825+FelipeFlorencio9@users.noreply.github.com>
2023-10-26 08:58:23 -0300

    Testando a main

--- app/src/main/java/br/senai/sp/jandira/limpeanapp/home/HomeScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/home/HomeScreen.kt
@@ -2,11 +2,17 @@ package br.senai.sp.jandira.limpeanapp.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import br.senai.sp.jandira.limpeanapp.feature_authentication.login.data.repository.TokenRepositoryImpl
+import br.senai.sp.jandira.limpeanapp.feature_authentication.login.di.LoginModule
+import br.senai.sp.jandira.limpeanapp.feature_authentication.login.domain.TokenRepository
 
 @Composable
 fun HomeScreen(
@@ -17,9 +23,23 @@ fun HomeScreen(
     Box {
         Column {
             Text(text = "Home Screen")
-            Text(text = viewModel.session.toString())
+            Text(text = state.token!!)
+            Button(onClick = {viewModel.getToken()}) {
+                Text(text = "Get Token")
+            }
         }
        
     }
 }
 
+@Preview
+@Composable
+fun HomePrev() {
+    val context = LocalContext.current
+    HomeScreen(
+        viewModel = HomeViewModel(tokenManager = TokenRepositoryImpl(
+            LoginModule.providePreferencesDataStore(context)
+        )
+        )
+    )
+}
