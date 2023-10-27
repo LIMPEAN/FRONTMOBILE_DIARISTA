c99b402fc2144bcb6191e23ea96ebb4e03fddafd
Felipe Florencio <107069825+FelipeFlorencio9@users.noreply.github.com>
2023-10-27 07:40:48 -0300

    Fazendo lista de diaristas

--- app/src/main/java/br/senai/sp/jandira/limpeanapp/home/data/remote/ListDiaristsResponse.kt
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/home/data/remote/ListDiaristsResponse.kt
@@ -1,9 +1,6 @@
 package br.senai.sp.jandira.limpeanapp.home.data.remote
 
 data class ListDiaristsResponse(
-    val status : Number,
-    val diarists: List<UserDiarist>,
+    val status : Number
 )
-data class UserDiarist(
 
-)
