0487e8090bd40661d3a1b6f9b445e5dbc75584a8
Felipe Florencio <107069825+FelipeFlorencio9@users.noreply.github.com>
2023-11-17 11:23:03 -0300

    feat: get services open but failed due to non-normalized address

--- app/src/main/java/br/senai/sp/jandira/limpeanapp/core/data/remote/DiaristApi.kt
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/core/data/remote/DiaristApi.kt
@@ -1,8 +1,13 @@
 package br.senai.sp.jandira.limpeanapp.core.data.remote
 
+import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.BaseDto
 import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.DiaristDto
 import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.GetDiaristDto
-import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.GetDiaristsDto
+import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.OpenServicesDto
+import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.ServiceDto
+import br.senai.sp.jandira.limpeanapp.core.data.remote.dto.UpdatePriceDTO
+import br.senai.sp.jandira.limpeanapp.core.domain.models.StatusService
+import okhttp3.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -13,21 +18,44 @@ interface DiaristApi {
     @GET("diarist")
     suspend fun getDiarist() : GetDiaristDto
 
-    @GET("diarists")
-    suspend fun getListOfDiarists() : GetDiaristsDto
-
     @PUT("diarist")
     suspend fun updateDiarist(diaristDto: DiaristDto)
-    
 
-    @GET("diarist/service")
+
+    //Pega todos os servicos em aberto do cliente
+    //TELA DE ENCONTRAR SERVICOS
+    @GET("client/service-open")
+    suspend fun getOpenServices() : OpenServicesDto
+
+
+    //Atualizar os status do serviço do cliente
+    // para recusar, mudar para rejeitado,
+    //PARA FINALIZAR : mudar para finalizar
+    @PUT("/diarist/schedule-service")
+    suspend fun putStatusService(
+        @Query("idService") idService : Number,
+        @Query("idStatus") idStatus: Number? = StatusService.AGENDADO.codigo
+    )
+
+
+    //Pega todos servicos do diarista, como convite. Ou pode filtrar pelo status: Em andamento, Finalizado ...
+    @GET("/diarist/service")
     suspend fun getInvites()
 
+    //Mesmo que pegar os serviços, mas pegando um pelo id
     @GET("diarist/service")
     suspend fun getInviteById(@Query("id") id : Number)
 
-
+    //Atualiza o preço do serviço
     @PUT("diarist/service/price")
-    suspend fun updatePrice()
+    suspend fun updatePrice(updatePriceInfo : UpdatePriceDTO) : BaseDto
 
+
+    //Pega o codigo do servico do cliente (para iniciar o serviço)
+    @GET("diarist/service/token")
+    suspend fun getTokenFromService(@Query("idService") idService: Number)
+
+//    @POST("assessment")
+//    suspend fun
+    //aaaaa
 }
---
+++ app/src/main/java/br/senai/sp/jandira/limpeanapp/core/data/remote/dto/UpdatePriceDTO.kt
@@ -0,0 +1,7 @@
+package br.senai.sp.jandira.limpeanapp.core.data.remote.dto
+
+data class UpdatePriceDTO(
+    val idService: Number,
+    val newValue : String
+)
+// Formato new Value : "190,00" , tratar como double
